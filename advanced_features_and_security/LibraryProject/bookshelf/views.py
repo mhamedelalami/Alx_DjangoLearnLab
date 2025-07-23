6ce94ba50833527a83f2db0cff8a521519c8c3a4
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-07-23 23:06:40 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,7 +5,7 @@ from .forms import BookForm  # Make sure you have this form created
 
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     """List all books, requires 'can_view' permission."""
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
