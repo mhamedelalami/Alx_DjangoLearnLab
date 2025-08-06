9f3cc363070176e7d10ab35152f196302c1a1054
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-06 20:15:00 +0100

    c

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import Filters, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework 
 from .models import Book
